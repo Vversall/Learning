7ee80c72913bfc33caa99fe5b9bfb8d721b79b36
Vversall <jydaidyuk@gmail.com>
2022-08-18 21:14:21 +0300

    format

--- 4/js/idk.js
+++ 4/js/idk.js
@@ -1,28 +1,24 @@
-async function getResponse(){
+async function getResponse() {
     let response = await fetch('https://queenlyrain.backendless.app/api/data/items')
     let content = await response.json()
     console.log(content)
-    let list = document.querySelector('.product_honey-item-container')
 
+    let list = document.querySelector('.product_honey-item-container')
     let key;
 
-    for(key in content){
+    for (key in content) {
         list.innerHTML += `
-
         <div class="product_honey-item">
         <div class="star"></div>
         <span class="product_honey-type">${content[key].name}</span>
         <div class="product_honey-about-wrapper">
             <img src="${content[key].imageUrl}" alt="honey-1.png">
             <span class="product_honey-price">${content[key].price} $ </span>
-            
             <a href="med.html">Замовити</a>
         </div>
     </div>
-
-
         `
-        content[key]
+    content[key]
     }
 }
 getResponse()
--- 4/js/idk2.js
+++ 4/js/idk2.js
@@ -1,14 +1,13 @@
-async function getResponse(){
+async function getResponse() {
     let response = await fetch('https://queenlyrain.backendless.app/api/data/tools')
     let content = await response.json()
     console.log(content)
-    let list = document.querySelector('.product_honey-item-container')
 
+    let list = document.querySelector('.product_honey-item-container')
     let key;
 
-    for(key in content){
+    for (key in content) {
         list.innerHTML += `
-
         <div class="product_honey-item">
         <div class="star"></div>
         <span class="product_honey-type">${content[key].name}</span>
@@ -17,11 +16,9 @@ async function getResponse(){
             <span class="product_honey-price">${content[key].price} </span>
             <a href="med.html">Замовити</a>
         </div>
-    </div>
-
-
+        </div>
         `
-        content[key]
+    content[key]
     }
 }
 getResponse()
